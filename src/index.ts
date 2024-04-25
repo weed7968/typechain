e4319fe96f591fa8ef09969908f401ecac18c7d8
weed7968 <tlstjdcjf980809@gmail.com>
2024-04-25 22:18:54 +0900

    JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { init, exit } from "myPackage";
+import { init, exit } from "./myPackage";
 
-init({
-  url: "true",
-});
-
-exit(1);
-
-localStorage.clear();
+init();
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,20 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {boolean} config.url
+ * @returns
+ */
 export function init(config) {
   return true;
 }
 
+/**
+ * Exits the profram
+ * @param {number} code
+ * @returns
+ */
 export function exit(code) {
   return code + 1;
 }
